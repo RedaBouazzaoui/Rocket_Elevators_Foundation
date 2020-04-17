cedb9442c2c0be71dc684158958dcef04b8454cd
RdaBouazzaoui <reda.bouazaoui10@gmail.com>
2020-04-17 15:49:52 -0400

    recaptcha leads

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -26,7 +26,7 @@ class LeadsController < ApplicationController
     
 
     #render json: @lead #test when submit button form
-    if @lead.save
+    if verify_recaptcha()  && @lead.save
 
       ContactsMailer.contact_email(@lead).deliver
       flash[:notice] = "We received your request! "
